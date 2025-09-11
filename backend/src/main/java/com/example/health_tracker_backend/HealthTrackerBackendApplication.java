f3b49e418c00b9e036ce956f9b4f7f0ba17fe914
Catherine Xie <catherinexie2014@gmail.com>
2025-09-10 22:49:31 -0400

    setup java spring boot maven project as backend and javascript frontend

---
+++ backend/src/main/java/com/example/health_tracker_backend/HealthTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.health_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/health_tracker_backend/HealthTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.health_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
