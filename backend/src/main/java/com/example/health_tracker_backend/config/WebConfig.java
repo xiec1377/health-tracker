a45197943c4a6bf0d668c48652ea06771fabe00f
Catherine Xie <catherinexie2014@gmail.com>
2025-09-16 19:54:03 -0400

    create react project to migrate

--- backend/src/main/java/com/example/health_tracker_backend/config/WebConfig.java
+++ backend/src/main/java/com/example/health_tracker_backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") 
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5173/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
